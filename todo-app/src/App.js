633596493211d1ab3e3792d8fe0315e6d2773742
DeAndre Johnson <dre@phreshr.com>
2019-11-06 06:09:44 +0800

    add reducer logic

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react';
+import styled from 'styled-components/macro';
+
+import GlobalStyles from './styles/GlobalStyles';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        ...state,
+        todos: action.payload.todos,
+      };
+    case 'COMPLETE_TODO':
+      return {
+        ...state,
+      };
+    case 'CLEAR_TODO':
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const initialState = {
+    todos: [
+      {
+        item: 'Learn about reducers',
+        completed: false,
+        id: Date.now(),
+      },
+    ],
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addTodo = title => {
+    const newTodo = {
+      id: Date.now(),
+      item: title,
+      completed: false,
+    };
+    dispatch({
+      type: 'ADD_TODO',
+      payload: { todos: [...state.todos, newTodo] },
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+      <div className='App'>
+        <TodoForm addTodo={addTodo} />
+        <TodoList todos={state.todos} />
+      </div>
+    </>
   );
 }
 
